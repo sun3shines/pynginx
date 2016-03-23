874ef5bc4a07b9e6ff0be7b6dbc2805002578e54
root <sun__shines@163.com>
2016-03-23 17:12:50 +0800

    random static

--- pynginx/schedule/hash.py
+++ pynginx/schedule/hash.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+
+from pynginx.schedule.base import Schedule as BaseSchedule
+import time
+from pynginx.client.stream import Stream
+
+class Schedule(BaseSchedule):
+    
+    def index(self,value):
+        if self.lock.acquire():
+            if 0 == self.length:
+                self.lock.release()
+                return -1
+            
+            return 0 
+
+    def get(self,value):
+        sock = None
+        while True:
+            i = self.index(value)
+            while -1 == i:
+                # 如果没有可用server，则会一直等待了。
+                print 'wait for server'
+                time.sleep(self.interval)
+                i = self.index(value)
+            
+            host,port = self.cache[i]
+            print host,port
+            try:
+                sock = Stream(host,port).sock
+            except:
+                self.delete((host,port))
+                continue
+            break
+        return sock
+    
--- pynginx/schedule/random.py
+++ pynginx/schedule/random.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+from pynginx.schedule.base import Schedule as BaseSchedule
+import random
+
+class Schedule(BaseSchedule):
+    @property
+    def index(self):
+        if self.lock.acquire():
+            if 0 == self.length:
+                self.lock.release()
+                return -1
+            
+            return random.randint(0,self.length-1) 
